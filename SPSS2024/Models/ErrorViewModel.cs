8fdcee22b7256961c790a9bdd37ce3c79606aad7
tanlx <tanlx@huongvietgroup.com>
2023-10-24 15:04:03 +0700

    first commit

---
+++ SPSS2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SPSS2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
